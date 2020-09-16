4d260eca9d1d13fd1145ab39887aba915cac7248
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-16 09:01:44 +0200

    Konstruktor létrehozása

--- 20200916Kiralynok/Program.cs
+++ 20200916Kiralynok/Program.cs
@@ -10,7 +10,7 @@ namespace _20200916Kiralynok
     {
         class Tabla
         {
-            char[,] T = new char[8,8];
+            char[,] T;
             char UresCella;
             int UresoszlopokSzama, UressorokSzama;
             public void Elhelyez()
@@ -26,11 +26,25 @@ namespace _20200916Kiralynok
 
             }
             public Tabla(char ch)
-            { 
-                
+            {
+                T = new char[8, 8];
+                UresCella = ch;
+                for (int i = 0; i < T.GetLength(0); i++)
+                {
+                    for (int y = 0; y < T.GetLength(1); y++)
+                    {
+                        T[i, y] = UresCella;
+                    }
+                }
             }
-            public int UresOszlop;
-            public int UresSor;
+            public int UresOszlop()
+            {
+                return 0;
+            }
+            public int UresSor()
+            {
+                return 0;
+            }
         }
         static void Main(string[] args)
         {
