b6548bf06fbff8abd66702216dec5a6b9a869daf
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-09-16 08:29:06 +0200

    Add project files.

---
+++ 20200916Kiralynok/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200916Kiralynok
+{
+    class Program
+    {
+        class Tabla
+        {
+            char[,] T = new char[8,8];
+            char UresCella;
+            int UresoszlopokSzama, UressorokSzama;
+            public void Elhelyez()
+            {
+
+            }
+            public void FajlbaIr()
+            {
+
+            }
+            public void Megjelenit()
+            {
+
+            }
+            public Tabla(char ch)
+            { 
+                
+            }
+            public int UresOszlop;
+            public int UresSor;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
